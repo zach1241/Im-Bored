96080f61946199043645fae0c72121ea5a88b261
Zachary Swanepoel <95292855+zach1241@users.noreply.github.com>
2025-08-28 22:50:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
